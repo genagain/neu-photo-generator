ef457ad28561f664bb229160c1101b6591383f69
genagain <ohta.g@husky.neu.edu>
2016-07-08 18:40:42 -0400

    changed from dicts to strings for the user and album objects for testing

--- app/index.py
+++ app/index.py
@@ -27,8 +27,8 @@ def auth():
     testing = open('testing.txt', 'w+')
     user = graph.get_object(id=user_id, fields='id,name,education')
     albums = graph.get_connections(id=user_id,connection_name='albums')
-    testing.write('user' + user)
-    testing.write('album' + album)
+    testing.write('user' + str(user))
+    testing.write('album' + str(album))
     profile_album_id = [album for album in albums['data'] if album['name'] == 'Profile Pictures'][0]['id']
     profile_picture_object = graph.get_object(id=profile_album_id,fields='cover_photo')
     profile_picture_id = profile_picture_object['cover_photo']['id']
