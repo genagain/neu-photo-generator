4ce73bc84e8b1df31852182c70b3f760f2a0bf84
genagain <ohta.g@husky.neu.edu>
2016-07-16 22:02:47 -0400

    created largest image method

--- app/index.py
+++ app/index.py
@@ -20,13 +20,21 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
 
 def get_prof_pics(graph, user_id):
-  user = graph.get_object(id=user_id, fields='id,name,education')
   albums = graph.get_connections(id=user_id,connection_name='albums')
   profile_album_id = [album for album in albums['data'] if album['name'] == 'Profile Pictures'][0]['id']
   profile_picture_object = graph.get_object(id=profile_album_id,fields='cover_photo')
   profile_picture_id = profile_picture_object['cover_photo']['id']
   profile_pictures_images = graph.get_object(id=profile_picture_id,fields='images')
   return profile_pictures_images['images']
+
+def largest_img(profile_pictures):
+  max_width = 0
+  profile_picture = ''
+  for picture in profile_pictures:
+    if picture['width'] > max_width:
+      max_width = picture['width']
+      profile_picture = picture
+  return profile_picture
 
 @app.route('/', methods=['GET','POST'])
 def home():
@@ -37,14 +45,9 @@ def auth():
   token = str(request.form['accessToken'])
   user_id = request.form['userID']
   graph = facebook.GraphAPI(access_token=token, version='2.5')
+  user = graph.get_object(id=user_id, fields='id,name,education')
   profile_pictures = get_prof_pics(graph, user_id)
-  max_width = 0
-  profile_picture = ''
-  for picture in profile_pictures:
-    if picture['width'] > max_width:
-      max_width = picture['width']
-      profile_picture = picture
-
+  profile_picture = largest_img(profile_pictures)
   gs_file = 'gs-' + user['name'].replace(" ", "") + '.jpg'
   response = requests.get(profile_picture['source'])
   gs_image = Image.open(BytesIO(response.content)).convert('L')
