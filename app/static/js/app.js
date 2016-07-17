9a9ace7ccb1781151f69a64559c257876f77d4d5
mikehwagz <hmiklw@me.com>
2016-07-16 22:49:53 -0400

    create retina friendly canvas rendering of poster

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -2,58 +2,25 @@
 
 $(document).ready(function () {
 
-  $('.js-input').on('keyup', function () {
-    var user_input = $('.js-input').val(),
-        elem = $('.js-quote');
-    elem.html(user_input);
-    return user_input;
-  });
+    $('.js-input').on('keyup', function () {
+        var user_input = $('.js-input').val(),
+            elem = $('.js-quote');
+        elem.html(user_input);
+        return user_input;
+    });
 
-  $('.js-button').on('click', function () {
-    var elem = $('.js-quote'),
-        user_input = $('.js-input').val();
+    $('.js-button').on('click', function () {
+        var elem = $('.js-quote'),
+            user_input = $('.js-input').val();
 
-    elem.html(user_input);
+        elem.html(user_input);
 
-    var words = user_input.split(' '),
-        index = Math.floor(Math.random() * (words.length - 1)),
-        wordA = words[index],
-        wordB = words[index + 1],
-        randomWords = wordA + ' ' + wordB;
+        var words = user_input.split(' '),
+            index = Math.floor(Math.random() * (words.length - 1)),
+            wordA = words[index],
+            wordB = words[index + 1],
+            randomWords = wordA + ' ' + wordB;
 
-    elem.html(elem.html().replace(new RegExp(randomWords, 'g'), '<strong>' + randomWords + '</strong>'));
-  });
-  //
-  // onBtnEvent('mouseenter', BtnMouseEnter);
-  // onBtnEvent('mouseleave', BtnMouseLeave);
-});
-
-// end doc ready
-
-var fbPostBtn = $('.js-button--fb');
-
-fbPostBtn.on('click', function () {
-  html2canvas(document.querySelector('.js-poster')).then(function (canvas) {
-    console.log(canvas, 'click!', $('.js-input').val());
-    $('.js-ghost').append(canvas);
-
-    var imgData = document.querySelector('canvas').toDataURL('image/png');
-  });
-});
-
-var onBtnEvent = function onBtnEvent(event, callback) {
-  var button = $('.js-button--fb');
-  button.on(event, callback);
-};
-
-var BtnMouseEnter = function BtnMouseEnter(e) {
-  var target = $(e.currentTarget),
-      animation = target.data('hover');
-  target.addClass(animation);
-};
-
-var BtnMouseLeave = function BtnMouseLeave(e) {
-  var target = $(e.currentTarget),
-      animation = target.data('hover');
-  target.removeClass(animation);
-};
+        elem.html(elem.html().replace(new RegExp(randomWords, 'g'), '<strong>' + randomWords + '</strong>'));
+    });
+}); // end doc ready
--- src/js/app.js
+++ src/js/app.js
@@ -22,46 +22,5 @@ $(document).ready( () => {
       elem.html(elem.html().replace(new RegExp( randomWords, 'g' ),'<strong>' + randomWords + '</strong>'));
 
     });
-  //
-  // onBtnEvent('mouseenter', BtnMouseEnter);
-  // onBtnEvent('mouseleave', BtnMouseLeave);
-
-});
-
-// end doc ready
-
-let fbPostBtn = $('.js-button--fb');
-
-
-fbPostBtn.on('click', () => {
-  html2canvas(document.querySelector('.js-poster'))
-    .then(function(canvas) {
-      console.log(canvas,'click!', $('.js-input').val())
-      $('.js-ghost').append(canvas);
-
-      let imgData = document.querySelector('canvas').toDataURL('image/png');
-
-  });
-});
-
-
-
-
-
-
-let onBtnEvent = (event, callback) => {
-	let button = $('.js-button--fb');
-	button.on(event, callback);
-}
-
-let BtnMouseEnter = (e) => {
-  let target = $(e.currentTarget),
-  animation = target.data('hover');
-  target.addClass(animation);
-}
-
-let BtnMouseLeave = (e) => {
-  let target = $(e.currentTarget),
-  animation = target.data('hover');
-  target.removeClass(animation);
-}
+    
+}); // end doc ready
